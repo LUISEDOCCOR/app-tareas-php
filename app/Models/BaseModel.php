ac691ba23e283bf62f9ebda762803276b0a65932
Luis Eduardo Ocegueda <luis@eduardoocegueda@gmail.com>
2025-07-22 23:00:02 -0600

    feat: add baseModel

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,77 @@
+<?php
+namespace App\Models;
+
+use mysqli;
+
+class BaseModel {
+    protected $tabla = "";
+    protected $campos = [];
+    
+    protected static function conectarDB () {
+        $conn = new mysqli("localhost", "root", "12345678", "app_tareas");
+        return $conn;
+    }
+
+    protected function obtenerTodos () {
+        $conn = self::conectarDB();
+        $stmt = $conn->prepare("SELECT * FROM ". $this->tabla);
+        $stmt->execute();
+        $result= $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    protected function obtenerById ($id) {
+        $conn = self::conectarDB();
+        $stmt = $conn->prepare("SELECT * FROM ".$this->tabla." WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
+    }
+
+    protected function borrar ($id) { 
+        $conn = self::conectarDB();
+        $stmt = $conn->prepare("DELETE FROM ".$this->tabla." WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        return $stmt->affected_rows;
+    }
+
+    protected function crear ($valores) { 
+        $conn = self::conectarDB();
+        
+        $camposFormateados = "";
+        foreach($this->campos as $campo) {
+            $camposFormateados .= $campo . ", ";
+        }
+        $camposFormateados = rtrim($camposFormateados, ", ");
+        $valoresFormateados = str_repeat("?, ", count($valores));
+        $valoresFormateados = rtrim($valoresFormateados, ", ");
+        
+        $stmt = $conn->prepare("INSERT INTO $this->tabla ($camposFormateados) VALUES ($valoresFormateados)");
+        $stmt->bind_param(str_repeat("s", count($valores)), ...$valores);
+        $stmt->execute();
+        return $stmt->affected_rows;
+    }
+
+     public function update($id, $datos)
+    {
+        $conn = self::conectarDB();
+
+        $campos = "";
+        $valores = [];
+        foreach ($datos as $key => $value) {
+            $campos .= "$key = ?, ";
+            $valores[] = $value;
+        }
+        $campos = rtrim($campos, ", ");
+
+        $stmt = $conn->prepare("UPDATE $this->tabla SET $campos WHERE id = ?");
+
+        $tipos = str_repeat("s", count($valores)) . "i";
+        $valores[] = $id;
+
+        $stmt->bind_param($tipos, ...$valores);
+        return $stmt->execute();
+    }
+}
--- public/index.php
+++ public/index.php
@@ -9,17 +9,3 @@ if(!isset($_SESSION["appController"])){
     $_SESSION["appController"] = $appController;
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/output.css">
-    <title>Document</title>
-</head>
-<body>
-    <h1 class="text-8xl">Prueba estilos</h1>
-    <?= $_SERVER["REQUEST_URI"]?>
-</body>
-</html>
